6dd005a57c9880d34da2de9f00023e74b7dc2382
Celal <celal51@gmail.com>
2022-04-05 03:03:09 +0300

    new version 0.0.2
    Added delete feature

--- Tree/BSTree/App.java
+++ Tree/BSTree/App.java
@@ -6,16 +6,27 @@ public class App {
     public static void main(String[] args) {
         BSTree bt = new BSTree();
 
-        bt.rootNode =  bt.Insert(bt.rootNode, 10);
-        bt.rootNode =  bt.Insert(bt.rootNode, 15);
-        bt.rootNode =  bt.Insert(bt.rootNode, 8);
-        bt.rootNode =  bt.Insert(bt.rootNode, 20);
-        bt.rootNode =  bt.Insert(bt.rootNode, 4);
-        bt.rootNode =  bt.Insert(bt.rootNode, 12);
-
-        System.out.println("Root : " + bt.rootNode.data);
-        System.out.println("Root right : " + bt.rootNode.rightNextNode.data);
         
-    }
+
+        bt.Add(10);
+        bt.Add(8);
+        bt.Add(15);
+        bt.Add(5);
+        bt.Add(12);
+        bt.Add(20);
+        bt.Add(9);
+        bt.Add(25);
+        bt.Add(30);
+        bt.inOrder(bt.rootNode);
+        bt.Delete(bt.rootNode, 10);
+        System.out.println("--------------");
+        bt.inOrder(bt.rootNode);
+
+
+    }   
+
+    
+ 
+    
     
 }
